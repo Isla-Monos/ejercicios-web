1b541243159374ffcf8f840b699633c5cd57aee5
danisnake1000 <adriandanielfernandez@gmail.com>
2022-01-21 17:51:14 -0300

    que comience el juego
    Que sea lo que Dios quiera

--- calculadora-de-propinas/app.js
+++ calculadora-de-propinas/app.js
@@ -1,1 +1,29 @@
-console.log('acá va la magia')
+
+function calcular(){
+	
+var montoIni =0;
+// var propina =0;
+var valorPropina = 0;
+var total =0;
+
+
+montoIni = document.getElementById('totalFacturado').value;
+// valorPropina = document.getElementById('importe_propina').value;♥
+
+
+valorPropina = (montoIni / 10);
+
+document.getElementById('importePropina').value=valorPropina;
+
+total = montoIni + valorPropina;
+
+document.getElementById('total').value=total;
+console.log(total);
+}
+
+
+
+
+
+
+// console.log('acá va la magia')
